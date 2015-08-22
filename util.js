ff80c1539a0b9fafb35a14321e0331aa13f542bf
TomNaN <tomna.git@gmail.com>
2015-08-22 12:33:52 +0300

    Added map drawing methods and other various stuff

--- util.js
+++ util.js
@@ -1,24 +1,47 @@
 var canvas,
-  ctx;
+    ctx,
+    xmlhttp = new XMLHttpRequest(),
+    listeners = [readFile],
+    mapData;
 function begin(){
+  getFile("resources/level.txt")
   canvas = document.getElementById("gameCanvas");
   ctx = canvas.getContext("2d");
-  tiles.push(new TileBasic(50,50));
-  entitys.push(new Hero(200, 200, 10, 10));
+  drawMap(mapData);
   setInterval(game, 1000/60);
 
 }
 function game() {
   ctx.clearRect(0, 0, 640, 480);
+  ctx.fillStyle = "#1122FF";
+  ctx.fillRect(0,0,640,480);
   //TODO: Järjestys olisi jotakin: tarkista tuleeko komentoja (käyttäjän liike + AI:n liike) -> tarkista törmäykset -> liiku jos mahdollista -> piirrä.
   for(var i=0; i<entitys.length; i++) {
     checkCollision();
-	entitys[i].move();
+	  entitys[i].move();
     entitys[i].draw();
-	entitys[i].spriteX+=0.2;
+	  entitys[i].spriteX+=0.2;
   }
-  for(var i=0; i<tiles.length; i++){  
-    tiles[i].draw();
-  }
-  
+  tiles.forEach(function(e){e.draw()}); 
+}
+function getFile(file){
+  listeners.forEach(function(e){xmlhttp.removeEventListener("readystate",e)});
+  xmlhttp.addEventListener("readystatechange",readFile);
+  xmlhttp.open("GET",file,false);
+  xmlhttp.send(); 
+}
+function readFile(){
+  mapData = xmlhttp.readyState==4?xmlhttp.responseText:null;
+}
+function drawMap(text){
+  map = text.replace(/\r/g,"").split('\n'); // FOR SOME REASON text HAD AN ODD \r
+  map.forEach(function(e,y){
+    e.split('').forEach(function(e,x){
+      switch(+e){
+        case 1:tiles.push(new TileBasic(x*spriteWidth, y*spriteWidth));break;    
+        case 2:entitys.push(new Hero(x*spriteWidth, y*spriteWidth, 10, 10));break;
+        default:tiles.push(new TileAir(x*spriteWidth, y*spriteWidth));
+      }
+    })
+  });
 }
