96637a6a9fe47956be4ee8a2473a2c94dc4fce91
TomNaN <tomna.git@gmail.com>
2015-08-24 22:20:56 +0300

    Added bgm and various other sounds

--- sounds.js
+++ sounds.js
@@ -1,1 +1,4 @@
 var sfxJump = new Audio("resources/playerJump.wav");
+var bgm = new Audio("resources/bgmusic.wav");
+var deadLantern = new Audio("resources/deadLantern.wav");
+var deadPlayer = new Audio("resources/deadPlayer.wav");
